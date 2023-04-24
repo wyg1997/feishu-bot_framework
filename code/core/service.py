f3256690be51cc3e96a257431629644db717f8a4
wyg1997 <wyg19970408@gmail.com>
2023-04-24 19:24:18 +0800

    refactor(message): refine message_reply and error uploading

---
+++ code/core/service.py
@@ -0,0 +1,20 @@
+import logging
+from larksuiteoapi.service.im.v1 import model
+
+from core.data_structure import MsgInfo, MessageType
+from core.config import service
+
+
+def reply_message(msg_info: MsgInfo, content: str):
+    body = model.MessageReplyReqBody()
+    body.content = content
+    body.msg_type = MessageType.text.name
+
+    req_call = service.messages.reply(body)
+    req_call.set_message_id(str(msg_info.msg_id))
+
+    resp = req_call.do()
+    if resp.code != 0:
+        logging.error(
+            f"reply message failed, code: {resp.code}, msg: {resp.msg}, error: {resp.error}"
+        )
--- code/handlers/helper_handle.py
+++ code/handlers/helper_handle.py
@@ -1,23 +1,11 @@
 import logging
-from larksuiteoapi.api import Request
-from larksuiteoapi.service.im.v1 import model
 
 from handlers.register import msg_handle_register
-from core.data_structure import MsgInfo, MessageType
-from core.config import sdk_config, service
+from core.data_structure import MsgInfo
+from core.service import reply_message
 
 
 @msg_handle_register.register_handle(keys=["/help", "/start", "帮助"])
 def helper_handle(msg_info: MsgInfo):
-    body = model.MessageReplyReqBody()
-    body.content = '{"text": "Hello world!"}'
-    body.msg_type = MessageType.text.name
-
-    req_call = service.messages.reply(body)
-    req_call.set_message_id(str(msg_info.msg_id))
-
-    resp = req_call.do()
-    if resp.code != 0:
-        logging.error(
-            f"reply message failed, code: {resp.code}, msg: {resp.msg}, error: {resp.error}"
-        )
+    logging.info(f"helper_handle, msg_info: {msg_info}")
+    reply_message(msg_info, '{"text": "Hello world!"}')
