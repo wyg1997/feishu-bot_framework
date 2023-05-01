70f23e705854d9b570fe8543bcc163d51bb1b431
wyg1997 <wyg19970408@gmail.com>
2023-05-01 17:02:02 +0800

    style(ChatStyle): refine code

--- code/handlers/all/chat_style_handle.py
+++ code/handlers/all/chat_style_handle.py
@@ -11,39 +11,32 @@ from core.service import reply_message
 from core.tools import get_conversation_style_string
 
 
+def _get_chat_style_card():
+    card_builder = CardBuilder()
+    card_builder.add_header(CardTemplate.blue, title="请选择对话风格")
+
+    buttons = []
+    for style in ConversationStyle:
+        buttons.append(
+            Button(
+                text=get_conversation_style_string(style),
+                url="",
+                type=Button.ButtonType.primary
+                if global_config["CONVERSATION_STYLE"] == style
+                else Button.ButtonType.default,
+                value={"conversation_style": style.name},
+            )
+        )
+    card_builder.add_button_group(buttons)
+    card_builder.add_note(
+        note_content=f"当前模式: {get_conversation_style_string(global_config['CONVERSATION_STYLE'])}"
+    )
+    return card_builder
+
+
 @msg_handle_register.register_object(key=["/chat_style"])
 def chat_style_handle(msg_info: MsgInfo):
-    logging.info(f"bing_chat_handle, msg_info: {msg_info}")
-    bot_pool.ask(msg_info, ActionType.chat_style)
-
-
-@bot_register.register_object(key=ActionType.chat_style)
-class ChatStyleHandler(BotBase):
-    def _do(self, msg_info: MsgInfo):
-        card_builder = CardBuilder()
-        # first message
-        if len(self.message_ids) == 1:
-            card_builder.add_header(CardTemplate.blue, title="请选择对话风格")
-
-            buttons = []
-            for style in ConversationStyle:
-                buttons.append(
-                    Button(
-                        text=get_conversation_style_string(style),
-                        url="",
-                        type=Button.ButtonType.primary
-                        if global_config["CONVERSATION_STYLE"] == style
-                        else Button.ButtonType.default,
-                        value={"conversation_style": style.name},
-                    )
-                )
-            card_builder.add_button_group(buttons)
-        else:
-            card_builder.add_markdown("在上面选择模式吧，和我说没用哦🤪")
-        card_builder.add_note(
-            note_content=f"当前模式: {get_conversation_style_string(global_config['CONVERSATION_STYLE'])}"
-        )
-        reply_message(msg_info.msg_id, card_builder.build())
+    reply_message(msg_info.msg_id, _get_chat_style_card().build())
 
 
 @card_handle_register.register_object(key="conversation_style_card")
@@ -65,22 +58,4 @@ def conversation_style_card_handle(card):
     reply_message(msg_id, card_builder.build())
 
     # update card
-    card_builder = CardBuilder()
-    card_builder.add_header(CardTemplate.blue, title="请选择对话风格")
-    buttons = []
-    for style in ConversationStyle:
-        buttons.append(
-            Button(
-                text=get_conversation_style_string(style),
-                url="",
-                type=Button.ButtonType.primary
-                if global_config["CONVERSATION_STYLE"] == style
-                else Button.ButtonType.default,
-                value={"conversation_style": style.name},
-            )
-        )
-    card_builder.add_button_group(buttons)
-    card_builder.add_note(
-        note_content=f"当前模式: {get_conversation_style_string(global_config['CONVERSATION_STYLE'])}"
-    )
-    return card_builder.dict()
+    return _get_chat_style_card.dict()
